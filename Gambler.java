dc8e307d5a66d4d5866065cca3fa13e412b4243b
unknown <somesh.biradar95@gmail.com>
2022-07-17 20:49:08 +0530

    Created UC1 program to check win or loose

--- Gambler.java
+++ Gambler.java
@@ -1,14 +1,36 @@
+import java.util.Random;
 
 public class Gambler {
+	
 	
 	public static void main(String[] args) {
 		
 		int stake =100;
-		int day=0;
-		int bet=1;
+	
 		
+		Random rd=new Random();
+	    int RANDOM = rd.nextInt();
+	    int game=(RANDOM%2);
+
+			int remainingStake=stake;
+			
+				if(game ==0) {
+					System.out.println("You win 1$");
+	
+					remainingStake=remainingStake+1;
+					System.out.println(remainingStake);
+				}
+				else {
+					
+					System.out.println("You Loose 1$");
+					remainingStake=remainingStake-1;
+					System.out.println(remainingStake);
+				}
+			
+			
 		
+	  }
 		
-	}
+	
 
 }
