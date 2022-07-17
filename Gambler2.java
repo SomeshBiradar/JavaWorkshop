b6c623034b5177bbf33d202c69241e12797cc44e
unknown <somesh.biradar95@gmail.com>
2022-07-17 21:25:54 +0530

    Created second program for use case 3

---
+++ Gambler2.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Gambler2 {
+	
+public static void main(String[] args) {
+	
+	int stake =100;
+    int minStake=50;
+    int maxStake=150;
+    
+    Random rd=new Random();
+    int RANDOM = rd.nextInt();
+    int game=(RANDOM%2);
+ 
+	int remainingStake=stake;
+	
+		
+		
+		while(minStake==50) {
+			
+			if(game==1) {
+				
+			 
+			}				
+			
+		}
+			
+	
+}
+		
+		
+}
